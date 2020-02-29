b75ec2b0e26eccd8010bf75a845cd5d4c8ca5cb7
concurrent <concurrent.exec@gmail.com>
2020-02-29 16:02:49 +0800

    add map

--- theme/src/components/InteractiveModels.vue
+++ theme/src/components/InteractiveModels.vue
@@ -1,12 +1,72 @@
 <template>
-    <div class="visualizations">  
-    </div>    
+  <div class="interactive-models">
+    <canvas id="models" class="canvas"></canvas>
+  </div>
 </template>
 
+<script>
+import Chart from "chart.js";
+
+export default {
+  data() {
+    return {
+      modelData: {
+        type: "line",
+        data: {
+          labels: [],
+          datasets: []
+        },
+        options: {}
+      }
+    };
+  },
+  methods: {
+    canvasCtx() {
+      return document.getElementById("models").getContext("2d");
+    },
+    createModelLabels() {
+      const result = new Array(22);
+      for (let i = 0; i < result.length; i++) {
+        result[i] = i;
+      }
+      return result;
+    },
+    createModelDatasets() {
+      const result = {
+        label: "预测",
+        backgroundColor: "rgba(255, 255, 255, 0)",
+        borderColor: "rgba(100,157,205,1)",
+        data: []
+      };
+      const f = x => 100 - (x - 10) ** 2;
+      for (let i = 0; i <= 20; i+=1) {
+        result.data.push({ x: i, y: f(i) });
+      }
+      return result;
+    }
+  },
+  mounted() {
+    this.modelData.data.labels = this.createModelLabels();
+    this.modelData.data.datasets.push(this.createModelDatasets());
+    new Chart(this.canvasCtx(), this.modelData);
+  }
+};
+</script>
+
 <style scoped>
-.visualizations{
-    height: 400px;
-    background-color: #fff;
-    margin: 20px;
+.interactive-models {
+  padding: 10px;
+  height: 400px;
+  background-color: #fff;
+  margin: 20px;
+}
+.canvas {
+  width: 100%;
+  height: 100%;
+}
+@media (max-width: 767px) {
+  .interactive-models {
+    height: 300px;
+  }
 }
 </style>
--- theme/src/components/Visualizations.vue
+++ theme/src/components/Visualizations.vue
@@ -1,12 +1,24 @@
 <template>
-    <div class="interactive-models">  
+    <div class="visualizations">
+        <iframe class="visualizations-iframe" src="https://coronavirus.app/map?embed=true" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
     </div>    
 </template>
 
 <style scoped>
-.interactive-models{
-    height: 400px;
-    background-color: #fff;
-    margin: 20px;
+.visualizations {
+  height: 400px;
+  background-color: rgb(253, 253, 253);
+  margin: 10px 20px 30px 20px;
+  padding: 10px;
+}
+.visualizations-iframe{
+  width: 100%;
+  height: 100%;
+}
+@media (max-width: 767px) {
+  .visualizations {
+    height: 300px;
+  }
 }
 </style>
